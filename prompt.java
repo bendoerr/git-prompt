b4e8ac2910606e711f7a0f4ab3f2dcca5143c5bd
Ben Doerr <tiggerizzy@bendoerr.me>
2011-11-28 08:40:02 -0500

    More JGit

--- prompt.java
+++ prompt.java
@@ -1,31 +1,46 @@
-import org.eclipse.jgit.lib.*;
-import org.eclipse.jgit.dircache.*;
-import org.eclipse.jgit.treewalk.*;
-import java.io.*;
+import org.eclipse.jgit.lib.IndexDiff;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
+
+import java.io.File;
+import java.io.IOException;
 
 class Prompt {
-  public static void main(String[] args)
-      throws IOException {
-    System.out.println("Test");
+    public static void main(String[] args)
+            throws IOException {
+        System.out.println("Test");
 
-    Repository repo = new RepositoryBuilder()
-        .setGitDir(new File("./.git/"))
-        .readEnvironment()
-        .findGitDir()
-        .build();
+        Repository repo = new RepositoryBuilder()
+                .setGitDir(new File("./.git/"))
+                .readEnvironment()
+                .findGitDir()
+                .build();
 
-    if(repo.isBare()) { // No .git (might not even be a git repop)
-        System.out.println("No Repo");
-        return;
+        if (repo.isBare()) { // No .git (might not even be a git repop)
+            System.out.println("No Repo");
+            return;
+        }
+
+        String branch = repo.getBranch();
+        System.out.println("Branch: " + branch);
+
+        System.out.println(new FileTreeIterator(repo).getOptions().getAutoCRLF());
+
+        IndexDiff index = new IndexDiff(repo, "HEAD", new FileTreeIterator(repo));
+        index.diff();
+
+        System.out.println("Changed not staged: " + index.getModified());
+        System.out.println("Staged: " + index.getChanged());
+
+
+        RevWalk walk = new RevWalk(repo);
+        ObjectId from = repo.resolve("refs/heads/master");
+        ObjectId to = repo.resolve("refs/remotes/origin/master");
+
+        walk.markStart(walk.parseCommit(from));
+        walk.markUninteresting(walk.parseCommit(to));
     }
-
-    String branch = repo.getBranch();
-    IndexDiff index = new IndexDiff(repo, "HEAD", new FileTreeIterator(repo));
-    index.diff();
-
-    System.out.println("Branch: " + branch);
-    System.out.println("Changed not staged: " + index.getModified());
-    System.out.println("Staged: " + index.getChanged()); 
-    
-  }
 }
