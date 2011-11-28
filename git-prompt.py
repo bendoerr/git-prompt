169fb157e3864f948c9f84e046ee62f0be5fa9ad
Ben Doerr <tiggerizzy@bendoerr.me>
2011-11-28 11:59:11 -0500

    Colors and Prompt

--- git-prompt.py
+++ git-prompt.py
@@ -1,30 +1,44 @@
 from git import *
+from colorama import init, Fore, Back, Style
 
-repo = Repo()                                                                   #=> <git.Repo "/cygdrive/c/sanctuary/projects/git-prompt/.git">
+try:
+    repo = Repo()                                                               #=> <git.Repo "/cygdrive/c/sanctuary/projects/git-prompt/.git">
+except InvalidGitRepositoryError:
+    exit()
+
+init()
+
+remote_color = Style.BRIGHT + Fore.RED
 
 curr_b = repo.head.ref.name                                                     #=> 'master'
-curr_tb = repo.head.ref.tracking_branch()
+curr_tb = repo.head.ref.tracking_branch()                                       #=> <git.RemoteReference "refs/remotes/origin/master">
 
 if curr_tb:
-    curr_rb = curr_tb.name.split("/")                                               #=> {'origin', 'master'}
+    curr_rb = curr_tb.name.split("/")                                           #=> {'origin', 'master'}
 
-    curr_commit = repo.head.commit.hexsha                                           #=> '6b800a2fe00d33b3b953eda423438f03c3d59320'
-    curr_rb_commit = Git().ls_remote(curr_rb[0],
-                                     "refs/heads/" + curr_rb[1]).split("\t")[0]     #=> 'bf1e66aa5ec90325bf358469d1b13fac6777c045'
+    curr_commit = repo.head.commit.hexsha                                       #=> '6b800a2fe00d33b3b953eda423438f03c3d59320'
+    curr_r_commit = Git().ls_remote(curr_rb[0],
+                                    "refs/heads/" + curr_rb[1]).split("\t")[0]  #=> 'bf1e66aa5ec90325bf358469d1b13fac6777c045'
 
-    if curr_commit != curr_rb_commit:
-        # We differ. We might be ahead (curr_rb_commit would be in our history). We
-        # might be behind (curr_commit is in their history) or we might have
+    if curr_commit != curr_r_commit:
+        # We differ. We might be ahead (curr_rb_commit would be in our history).
+        # We might be behind (curr_commit is in their history) or we might have
         # diverged.
-        print("Out of sync")
+        remote_color = Fore.MAGENTA
     else:
-        print("Up to date")
+        remote_color = Fore.WHITE
 
-not_staged = bool(repo.index.diff(None))
-staged = bool(repo.index.diff('HEAD'))
+not_staged = repo.index.diff(None)
+staged = repo.index.diff('HEAD')
 
-if not_staged:
-    print("Has Items Not Staged")
+local_color = Fore.WHITE
 
-if staged:
-    print("Has Items Staged")
+if bool(staged):
+    if bool(not_staged):
+        local_color = Fore.YELLOW
+    else:
+        local_color = Fore.GREEN
+elif bool(not_staged):
+    local_color = Fore.RED
+
+print remote_color + "(" + Style.RESET_ALL + local_color + curr_b + Style.RESET_ALL + remote_color + ")" + Style.RESET_ALL
